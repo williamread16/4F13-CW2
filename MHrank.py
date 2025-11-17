f3a8c030789baf0a1b114f3d58765d83f2e0a3b5
williamread16 <billeread@gmail.com>
2025-11-17 13:49:05 +0000

    a

--- MHrank.py
+++ MHrank.py
@@ -11,7 +11,7 @@ def MH_sample(games, num_players, num_its):
     X = [[] for _ in range(num_players)] 
     for a, (i,j) in enumerate(games):
         X[i].append((j, +1))  # player i beat player j
-        X[j].append((i, -1))  # player j lost to payer i
+        X[j].append((i, -1))  # player j lost to player i
     for i in range(num_players):
         X[i] = np.array(X[i])
 
@@ -19,7 +19,7 @@ def MH_sample(games, num_players, num_its):
     skill_samples = np.zeros((num_players, num_its))
 
     w = np.zeros(num_players)  # skill for each player
-
+    accepted = 0  # count accepted moves
     for itr in tqdm(range(num_its)):
         for i in range(num_players):
             j, outcome = X[i].T
@@ -33,7 +33,27 @@ def MH_sample(games, num_players, num_its):
             # accept or reject move:
             # TODO
 
+            # 1. Propose a new skill w_prop from a Gaussian centered at the current skill w[i]
+            # A standard deviation of 0.1 is a reasonable starting point for tuning.
+            proposal_std = 0.1 
+            w_prop = w[i] + np.random.normal(0, proposal_std)
+            
+            # 2. Calculate the log-probability of the proposed skill
+            lp2 = norm.logpdf(w_prop) + np.sum(norm.logcdf(outcome*(w_prop-w[j])))
+
+            # accept or reject move:
+            # TODO
+            
+            # 3. Calculate the acceptance ratio in log-space
+            log_acceptance_ratio = lp2 - lp1
+            
+            # 4. Accept the move if the new state is more probable,
+            #    or with a probability corresponding to the acceptance ratio
+            if np.log(np.random.rand()) < log_acceptance_ratio:
+                accepted += 1
+                w[i] = w_prop
+            # (Else, do nothing and keep w[i] as it is, i.e., reject the move)
+
         skill_samples[:, itr] = w
 
-    return skill_samples
-
+    return skill_samples, accepted
